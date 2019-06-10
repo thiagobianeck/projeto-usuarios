9c4b89fe7dd7459fe3c21d5a576a9f919b0f2573
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-10 00:35:53 -0300

    33. G6 - IF e ELSE

---
+++ index.js
@@ -0,0 +1,26 @@
+var name = document.querySelector('#exampleInputName');
+var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
+var birth = document.querySelector('#exampleInputBirth');
+var country = document.querySelector('#exampleInputCountry');
+var email = document.querySelector('#exampleInputEmail');
+var password = document.querySelector('#exampleInputPassword');
+var photo = document.querySelector('#exampleInputFile');
+var admin = document.querySelector('#exampleInputAdmin');
+
+
+var fields = document.querySelectorAll('#form-user-create [name]');
+
+fields.forEach(function(field, index) {
+
+    if (field.name === "gender" && field.checked) {
+        console.log('SIM',field);
+    } else {
+        console.log('NÂO',field);
+    }
+
+    // console.log(field.id, field.name, field.value, field.checked, index);
+
+});
+
+
+
