109957410962e52c35dfd85bdb61ef1d1d9d9b21
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-13 19:35:44 -0300

    Trabalhando com JSON

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-var name = document.querySelector('#exampleInputName');
-var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
-var birth = document.querySelector('#exampleInputBirth');
-var country = document.querySelector('#exampleInputCountry');
-var email = document.querySelector('#exampleInputEmail');
-var password = document.querySelector('#exampleInputPassword');
-var photo = document.querySelector('#exampleInputFile');
-var admin = document.querySelector('#exampleInputAdmin');
-
-
 var fields = document.querySelectorAll('#form-user-create [name]');
+var user = {};
 
 fields.forEach(function(field, index) {
 
     if (field.name === "gender" && field.checked) {
-        console.log('SIM',field);
+        user[field.name] = field.value;
     } else {
-        console.log('NÂO',field);
+        user[field.name] = field.value;
     }
-
-    // console.log(field.id, field.name, field.value, field.checked, index);
-
 });
+
+console.log(user);
 
 
 
