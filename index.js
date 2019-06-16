b1d78a0f4f3e1f0d04d2b14f1ecf03c3e98f9913
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-16 14:30:18 -0300

    35. G8 - Trabalhando com Eventos no JavaScript

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var fields = document.querySelectorAll('#form-user-create [name]');
 var user = {};
 
-fields.forEach(function(field, index) {
 
-    if (field.name === "gender" && field.checked) {
-        user[field.name] = field.value;
-    } else {
-        user[field.name] = field.value;
-    }
+document.getElementById('form-user-create').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    fields.forEach(function(field, index) {
+
+        if (field.name === "gender" && field.checked) {
+            user[field.name] = field.value;
+        } else {
+            user[field.name] = field.value;
+        }
+    });
 });
 
 console.log(user);
-
-
-
