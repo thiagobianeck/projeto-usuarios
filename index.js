7367c55757fc403cb1e6231c33d3352221ed7896
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-16 16:57:38 -0300

    37. G10 - Programação Orientada a Objetos - Classes e MVC

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var fields = document.querySelectorAll('#form-user-create [name]');
 var user = {};
 
 function addLine(dataUser){
-    var tr = document.createElement("tr");
 
-    tr.innerHTML = `
+    console.log('dataUser =>', dataUser);
+
+    document.getElementById('table-users').innerHTML = `
             <tr>
                 <td>
                     <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
@@ -19,8 +20,6 @@ function addLine(dataUser){
                 </td>
             </tr>
     `;
-
-    document.getElementById('table-users').appendChild(tr);
 }
 
 
@@ -36,8 +35,18 @@ document.getElementById('form-user-create').addEventListener('submit', function 
         }
     });
 
-    addLine(user);
-    console.log(user);
+    var objectUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin
+    );
+
+    addLine(objectUser);
 
 });
 
