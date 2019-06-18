70a3d15e6d7e0fd1faf5828261a2bb3249501798
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-17 23:33:56 -0300

    42. G15 - Ajustando a View do Projeto - Operador Ternário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -22,7 +22,6 @@ class UserController {
                     console.error(e);
                 }
             );
-
         });
 
     }
@@ -48,7 +47,11 @@ class UserController {
                 reject(e);
             };
 
-            fileReader.readAsDataURL(file);
+            if(file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
         });
 
 
@@ -62,6 +65,8 @@ class UserController {
 
             if (field.name === "gender" && field.checked) {
                 user[field.name] = field.value;
+            } else if(field.name == 'admin'){
+                user[field.name] = field.checked;
             } else {
                 user[field.name] = field.value;
             }
@@ -83,21 +88,23 @@ class UserController {
 
         console.log('dataUser =>', dataUser);
 
-        this.tableEl.innerHTML = `
-            <tr>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
                 <td>
                     <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
                 </td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
+                <td>${dataUser.admin ? 'Sim' : 'Não'}</td>
                 <td>${dataUser.birth}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
-            </tr>
-    `;
+        `;
+
+        this.tableEl.appendChild(tr);
     }
 
 }
