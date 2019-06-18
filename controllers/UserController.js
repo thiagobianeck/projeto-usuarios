9f0c1405da27ef755f1bd1a9b538b1f1309ab651
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-17 23:20:38 -0300

    41. G14 - Trabalhando com Promise no JavaScript

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,31 +13,45 @@ class UserController {
             event.preventDefault();
             let values = this.getValues();
 
-            this.getPhoto((content) => {
-                values.photo = content;
-                this.addLine(values);
-            });
+            this.getPhoto().then(
+                (content) => {
+                    values.photo = content;
+                    this.addLine(values);
+                },
+                (e) => {
+                    console.error(e);
+                }
+            );
 
         });
 
     }
 
-    getPhoto(callback) {
-        let fileReader = new FileReader();
+    getPhoto() {
 
-        let elements = [...this.formEl.elements].filter(item => {
-            if (item.name === 'photo') {
-                return item;
-            }
+        return new Promise((resolve, reject) => {
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+                if (item.name === 'photo') {
+                    return item;
+                }
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+
+            fileReader.onerror = (e) => {
+                reject(e);
+            };
+
+            fileReader.readAsDataURL(file);
         });
 
-        let file = elements[0].files[0];
 
-        fileReader.onload = () => {
-            callback(fileReader.result);
-        };
-
-        fileReader.readAsDataURL(file);
     }
 
     getValues() {
