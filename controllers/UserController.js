d932e21cf12ed7343174d1bef019c7fe0cbee218
Thiago Bianeck <thiagobianeck@gmail.com>
2019-08-08 02:09:56 -0300

    Ajustes iniciais

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,9 @@
 class UserController {
 
-    constructor(formIdCreate, formIdUpdate, tableId) {
+    constructor(formSearch, inputUser) {
 
-        this.formEl = document.getElementById(formIdCreate);
-        this.formUpdateEl = document.getElementById(formIdUpdate);
-        this.tableEl = document.getElementById(tableId);
+        this.formSearch = document.getElementById(formSearch);
+        this.inputUserGithub = document.getElementById(inputUser);
         this.onSubmit();
         this.onEdit();
     }
@@ -15,237 +14,179 @@ class UserController {
                 this.showPanelCreate();
             });
 
-        this.formUpdateEl.addEventListener('submit', event => {
-           event.preventDefault();
-            let btn = this.formUpdateEl.querySelector('[type=submit]');
-
-            btn.disabled = true;
-
-            let values = this.getValues(this.formUpdateEl);
-
-            let index = this.formUpdateEl.dataset.trIndex;
-
-            let tr = this.tableEl.rows[index];
-
-            let userOld = JSON.parse(tr.dataset.user);
-
-            let result = Object.assign({}, userOld, values);
-
-            this.getPhoto(this.formUpdateEl).then(
-                (content) => {
-
-                    if(!values.photo) {
-                        result._photo = userOld._photo;
-                    } else {
-                        result._photo = content;
-                    }
-
-                    tr.dataset.user = JSON.stringify(result);
-
-                    tr.innerHTML = `
-                        <td>
-                            <img src="${result._photo}" alt="User Image" class="img-circle img-sm">
-                        </td>
-                        <td>${result._name}</td>
-                        <td>${result._email}</td>
-                        <td>${result._admin ? 'Sim' : 'Não'}</td>
-                        <td>${Utils.dateFormat(result._register)}</td>
-                        <td>
-                            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                        </td>
-                    `;
-
-                    this.addEventsTr(tr);
-
-                    this.updateCount();
-
-                    this.formUpdateEl.reset();
-
-                    btn.disabled = false;
-
-                    this.showPanelCreate();
-                },
-                (e) => {
-                    console.error(e);
-                }
-            );
-
-        });
     }
 
     onSubmit() {
 
-        this.formEl.addEventListener('submit', event => {
+        this.formSearch.addEventListener('submit', event => {
             event.preventDefault();
-
-            let btn = this.formEl.querySelector('[type=submit]');
-
-            btn.disabled = true;
-
-            let values = this.getValues(this.formEl);
-
-            if(!values) return false;
-
-            this.getPhoto(this.formEl).then(
-                (content) => {
-                    values.photo = content;
-                    this.addLine(values);
-                    this.formEl.reset();
-                    btn.disabled = false;
-                },
-                (e) => {
-                    console.error(e);
-                }
-            );
-        });
-
-    }
-
-    getPhoto(formEl) {
-
-        return new Promise((resolve, reject) => {
-            let fileReader = new FileReader();
-
-            let elements = [...formEl.elements].filter(item => {
-                if (item.name === 'photo') {
-                    return item;
-                }
+            this.getValues(this.formSearch).then(result => {
+                this.showCardUser();
+                this.prencheCard(result);
             });
 
-            let file = elements[0].files[0];
+            this.preencheDataTable();
 
-            fileReader.onload = () => {
-                resolve(fileReader.result);
-            };
 
-            fileReader.onerror = (e) => {
-                reject(e);
-            };
 
-            if(file) {
-                fileReader.readAsDataURL(file);
-            } else {
-                resolve('dist/img/boxed-bg.jpg');
-            }
         });
+
+    }
+
+    prencheCard(dados){
+        document.querySelector('#card-nome').innerHTML = dados.nome;
+        document.querySelector('#card-bio').innerHTML = dados.bio;
+        document.querySelector('#card-seguidores').innerHTML = dados.nro_seguidores;
+        document.querySelector('#card-seguindo').innerHTML = dados.nro_seguindo;
+        document.querySelector('#card-img').src = dados.imagem_url;
+        document.querySelector('#span-repos').innerHTML = dados.repositorios;
+    }
+
+    preencheDataTable() {
+        let dados;
+        this.getRepositories().then(result => {
+            dados = result;
+        }).finally(() => {
+            let table = $('#tbl-repositorios').DataTable();
+            table.destroy();
+            this.showDataTable();
+
+            table = $('#tbl-repositorios').DataTable({
+                data: dados,
+                columns: [
+                    {data: 'name',
+                        render: function(data, type, full, meta) {
+                            console.log({data, type, full, meta});
+                            let titulo = `${data}`.toUpperCase();
+                            return `<a href="${full.url}" target="_blank">${titulo}</a>`;
+                        }},
+                    {data: 'description'},
+                    // {data: 'language'},
+                    {data: 'language',
+                        class: 'text-center',
+                        render: function(data, type, full, meta) {
+                            if(data){
+                                let lang = '';
+                                switch (`${data}`.toLowerCase()) {
+                                    case 'java':
+                                        lang = 'fa-java';
+                                        break;
+                                    case 'html':
+                                        lang = 'fa-html5';
+                                        break;
+                                    case 'javascript':
+                                        lang = 'fa-js';
+                                        break;
+                                    case 'php':
+                                        lang = 'fa-php';
+                                        break;
+                                    case 'python':
+                                        lang = 'fa-python';
+                                        break;
+                                    default:
+                                        lang = '';
+                                }
+                                if(lang) {
+                                    return `<span style="font-size: 30px"><i class="fab ${lang}"></i></span>`;
+                                }
+                                return `${data}`.toUpperCase();
+                            }
+                            return '';
+                        }},
+                    {data: 'stars', class: 'text-center'},
+                ]
+                // 'paging': true,
+                // 'lengthChange': false,
+                // 'searching': false,
+                // 'ordering': true,
+                // 'info': true,
+                // 'autoWidth': false
+            });
+        });
+
 
 
     }
 
-    getValues(formEl) {
+    criaEstrelas(numeroEstrelas) {
+
+        let retorno = '';
+        let star = '<i class="fa fa-star"></i>'
+    }
+
+
+    async getValues(formSearch) {
 
         let user = {};
         let isValid = true;
 
-        [...formEl.elements].forEach((field, index) => {
-            if(['name','email','password'].indexOf(field.name) > -1 && !field.value){
-                field.parentElement.classList.add('has-error');
-                isValid = false;
+        let username = this.inputUserGithub.value;
+
+        if(!username){
+            this.inputUserGithub.parentElement.classList.add('has-error');
+            isValid = false;
+        } else {
+            this.inputUserGithub.parentElement.classList.remove('has-error');
+            let url = `https://api.github.com/users/${username}`;
+            let result = await Fetch.get(url);
+
+            if(result.name) {
+                Object.assign(user, {
+                    nome: result.name,
+                    imagem_url: result.avatar_url,
+                    bio: result.bio,
+                    nro_seguidores: result.followers,
+                    nro_seguindo: result.following,
+                    repositorios: result.public_repos
+                });
             }
 
-            if (field.name === "gender") {
-                if(field.checked) user[field.name] = field.value;
-            } else if(field.name == 'admin'){
-                user[field.name] = field.checked;
-            } else {
-                user[field.name] = field.value;
-            }
-
-        });
+        }
 
         if(!isValid){
             return false;
         }
 
-        return new User(
-            user.name,
-            user.gender,
-            user.birth,
-            user.country,
-            user.email,
-            user.password,
-            user.photo,
-            user.admin
-        );
+        return user;
+    }
+
+    async getRepositories() {
+        let repositories = [];
+        let username = this.inputUserGithub.value;
+        let url = `https://api.github.com/users/${username}/repos`;
+        let result = await Fetch.get(url);
+        result.forEach(repositorio => {
+
+            let objRepo = {
+                name: repositorio.name,
+                description: repositorio.description,
+                language: repositorio.language,
+                stars: repositorio.stargazers_count,
+                url: repositorio.html_url
+            };
+           repositories.push(objRepo);
+        });
+
+        return repositories;
     }
 
     addLine(dataUser) {
 
-        let tr = document.createElement('tr');
-        tr.dataset.user = JSON.stringify(dataUser);
-
-        tr.innerHTML = `
-                <td>
-                    <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
-                </td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin ? 'Sim' : 'Não'}</td>
-                <td>${Utils.dateFormat(dataUser.register)}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
-                </td>
-        `;
-
-        this.addEventsTr(tr);
-
-        this.tableEl.appendChild(tr);
-
-        this.updateCount();
     }
 
     addEventsTr(tr) {
 
-        tr.querySelector('.btn-delete').addEventListener('click', e => {
-            if(confirm('Deseja Realmente excluir?')){
-                tr.remove();
-                this.updateCount();
-            }
-        });
 
-        tr.querySelector('.btn-edit').addEventListener('click', e => {
-            let json = JSON.parse(tr.dataset.user);
-
-            this.formUpdateEl.dataset.trIndex = tr.sectionRowIndex;
-
-            for(let name in json) {
-                let field = this.formUpdateEl.querySelector(`[name="${name.replace('_','')}"]`);
-
-                if(field) {
-                    switch (field.type) {
-                        case 'file':
-                            continue;
-                            break;
-                        case 'radio':
-                            field = this.formUpdateEl.querySelector(`[name="${name.replace('_','')}"][value="${json[name]}"]`);
-                            field.checked = true;
-                            break;
-                        case 'checkbox':
-                            field.checked = json[name];
-                            break;
-                        default:
-                            field.value = json[name];
-                    }
-                }
-            }
-
-            this.formUpdateEl.querySelector('.photo').src = json._photo;
-
-            this.showPanelUpdate();
-        });
     }
 
-    showPanelCreate(){
-        document.querySelector('#box-user-create').style.display = 'block';
-        document.querySelector('#box-user-update').style.display = 'none';
+    showCardUser(){
+        document.querySelector('#card-user').style.display = 'block';
     }
 
-    showPanelUpdate(){
-        document.querySelector('#box-user-create').style.display = 'none';
-        document.querySelector('#box-user-update').style.display = 'block';
+    showDataTable(){
+        document.querySelector('#data-repos').style.display = 'block';
     }
+
 
     updateCount() {
 
